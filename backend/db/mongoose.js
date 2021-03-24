71b30ab90297d6e5e7270983d2756ef704ff46b7
João Paulo Rocha <jporocha@gmail.com>
2021-03-24 13:04:19 -0300

    Servindo estatico

---
+++ backend/db/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const connectionString=`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@tcc.uzrpa.mongodb.net/test`
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Falha na conexão:'));
+db.once('open', function() {
+  console.log('Conexão à base de dados estabelecida com sucesso...')
+});
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 require('dotenv/config')
+require('./db/mongoose')
 
 const express = require('express')
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/', express.static('./frontend/dist'))
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}.`)
