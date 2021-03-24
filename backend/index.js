3a6f5fa8651fe4af46d1046643cfe387075b3a4d
João Paulo Rocha <jporocha@gmail.com>
2021-03-24 13:07:30 -0300

    Serving static

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
-app.use('/', express.static('./frontend/dist'))
+app.use('/', express.static('frontend/dist'))
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}.`)
