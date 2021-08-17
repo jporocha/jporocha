25675aecb3598ac4d378ed0edb144f7c2f8c20d1
João Paulo Rocha <jporocha@gmail.com>
2021-08-17 14:40:06 -0300

    Commit do dist

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,39 +1,44 @@
-const result = require('dotenv').config({ path: './backend/config/.env' })
-if (result.error) console.log('Production mode (No env vars from .env file)')
+const result = require("dotenv").config({ path: "./backend/config/.env" });
+if (result.error) console.log("Production mode (No env vars from .env file)");
 
-const express = require('express')
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const passport = require('passport')
+const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const address = process.env.REDIRECT;
 
-const server = express()
+const server = express();
 
-server.use('/', express.static('./frontend/dist'))
+server.use("/", express.static("./frontend/dist"));
 
-const cors = require('cors')
-const compression = require('compression')
-const helmet = require('helmet')
+const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
 
-server.use(cookieParser())
-server.use(bodyParser.json())
-server.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {maxAge: 86400000}
-}))
-server.use(cors({
-  origin: 'http://localhost:8080',
-  credentials: true
-}))
+server.use(cookieParser());
+server.use(bodyParser.json());
+server.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 86400000 },
+  })
+);
+server.use(
+  cors({
+    origin: address,
+    credentials: true,
+  })
+);
 
-server.use(passport.initialize())
-server.use(passport.session())
-server.use(helmet())
-server.use(compression())
-server.use(express.json())
+server.use(passport.initialize());
+server.use(passport.session());
+server.use(helmet());
+server.use(compression());
+server.use(express.json());
 
-server.use('/api', require('../routes/api.js'));
+server.use("/api", require("../routes/api.js"));
 
-module.exports = server
+module.exports = server;
--- backend/routes/AuthRouter.js
+++ backend/routes/AuthRouter.js
@@ -1,20 +1,28 @@
-let passport = require('../config/passport')
-const express = require('express')
-const router = express.Router()
+let passport = require("../config/passport");
+const express = require("express");
+const router = express.Router();
+const address = process.env.REDIRECT;
 
-router.get('/facebook', 
-  passport.authenticate('facebook', { scope: ['email']}));
+router.get(
+  "/facebook",
+  passport.authenticate("facebook", { scope: ["email"] })
+);
 
-router.get('/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: 'http://localhost:8080/login', successRedirect:'http://localhost:8080/dashboard' }))
+router.get(
+  "/facebook/callback",
+  passport.authenticate("facebook", {
+    failureRedirect: `${address}/login`,
+    successRedirect: `${address}/dashboard`,
+  })
+);
 
-router.get('/logout', (req, res) => {
-  req.logout()
-  res.status(200).send('User has logged out...')
-})
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.status(200).send("User has logged out...");
+});
 
-router.get('/user', (req, res) => {
-  res.status(200).send(req.user)
-})
+router.get("/user", (req, res) => {
+  res.status(200).send(req.user);
+});
 
-module.exports = router
+module.exports = router;
